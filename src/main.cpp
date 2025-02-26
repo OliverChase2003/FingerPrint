792f06d35bb6635b0c1d11a8e4dd2f310be5e815
OliverChase2003 <oliverchase5408@gmail.com>
2025-02-26 16:08:29 +0800

    implement debug_print for serial0

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,32 @@
 #include <SD.h>
 #include <Adafruit_Fingerprint.h>
 
+// global var
+Adafruit_Fingerprint finger = Adafruit_Fingerprint(&Serial1);
+int num = 0;
+
+// declarations
+void debug_print(const char* fmt, ...);
+
 void setup() {
   // put your setup code here, to run once:
+  // debug uart port init
+  Serial.begin(115200);   // serial for debug
+  // fingerprint init
+  Serial1.begin(57600);   // serial for AS608
+  finger.begin(57600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+ 
 }
 
+void debug_print(const char* fmt, ...){
+  char buf[64];
+  va_list args;
+  va_start(args, fmt);
+  vsnprintf(buf, sizeof(buf), fmt, args);
+  va_end(args);
+  Serial.println(buf);
+}
