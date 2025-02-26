49afb602ec7cf5d430ce9897c21023c536287177
OliverChase2003 <oliverchase5408@gmail.com>
2025-02-26 14:33:25 +0800

    project initialize

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <SD.h>
+#include <Adafruit_Fingerprint.h>
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
