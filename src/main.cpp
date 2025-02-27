96de61c48b3a8011e9b7f0816dc46a8ca2d3bf8a
OliverChase2003 <oliverchase5408@gmail.com>
2025-02-27 16:39:03 +0800

    add some inits(untest), add panic print

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,33 @@
 #include <SPI.h>
 #include <SD.h>
 #include <Adafruit_Fingerprint.h>
+#include <Adafruit_SSD1306.h>
+
+// macros
+// ssd1306 defs
+#define SSD1306_WIDTH   128   // ssd1306 oled pixel width
+#define SSD1306_HEIGHT  64    // ssd1306 oled pixel height
+// spi0 defs
+#define CUSTOM_MOSI 23
+#define CUSTOM_MISO 19
+#define CUSTOM_SCK  18
+#define CUSTOM_SS   5
 
 // global var
-Adafruit_Fingerprint finger = Adafruit_Fingerprint(&Serial1);
+// fingerprint
+Adafruit_Fingerprint finger(&Serial1);
+// sdcard
+const int spi_chipSelect = 5;
+// ssd1306
+Adafruit_SSD1306 display(SSD1306_WIDTH, SSD1306_HEIGHT, &Wire, -1);
+// program's vars
 int num = 0;
 
 // declarations
 void debug_print(const char* fmt, ...);
+void panic(const char* s);
 
+// init func
 void setup() {
   // put your setup code here, to run once:
   // debug uart port init
@@ -17,6 +36,20 @@ void setup() {
   // fingerprint init
   Serial1.begin(57600);   // serial for AS608
   finger.begin(57600);
+  debug_print("fingerprint init success!");
+  // spi sdcard module init
+  SPI.begin(CUSTOM_SCK, CUSTOM_MISO, CUSTOM_MOSI, CUSTOM_SS);
+  if(SD.begin()){   // dont need to specify the spi bus, use spi0 as default
+    debug_print("sdcard init success!");
+  }else{
+    panic("sdcard init failed!");
+  }
+  // ssd1306 screen init
+  if(display.begin()){
+    debug_print("ssd1306 init success!");
+  }else{
+    panic("ssd1306 init failed!");
+  }
 }
 
 void loop() {
@@ -31,4 +64,9 @@ void debug_print(const char* fmt, ...){
   vsnprintf(buf, sizeof(buf), fmt, args);
   va_end(args);
   Serial.println(buf);
+}
+
+void panic(const char* s){
+  debug_print("panic: %s", s);
+  while(1);
 }
