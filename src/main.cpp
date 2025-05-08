3c4fd2519afe584a6e342b1266bd474a8198c22c
OliverChase2003 <oliverchase5408@gmail.com>
2025-05-08 18:56:19 +0800

    end

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,10 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <time.h>
-//#include <Adafruit_GFX.h>
 #include <Adafruit_Fingerprint.h>
 #include <U8g2lib.h>
 // std
+#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <string>
@@ -46,41 +46,39 @@ char bmap[256];
 // declarations
 // functions for debug
 void debug_print(const char *fmt, ...);   // tested
+int Serial2Int();
 void panic(const char *s);    // tested
-void ssd1306_print(int x, int y, const char *fmt, ...);   // tested
+void ssd1306_print(const char *fmt, ...);   // tested
 // wifi
-String wifi_addr_trans(int ip);
 void printLocalTime();
 int time2str(char *time_str);
+String wifi_addr_trans(int ip);
 // bitmaps
 void create_bitmap(const char *file);             // tested
 int read_bitmap(const char *file, char *bitmap);  // tested
 int write_bitmap(const char *file, char *bitmap); // tested
 void print_bitmap(char *bitmap);
 // log
-void check_in(const char *file, char *bitmap);
+void check_in(Adafruit_Fingerprint &finger, const char *bitmap_file);
 // fingerprints
 int finger_enroll(Adafruit_Fingerprint &finger, const char *bitmap_file);   //tested
 int finger_delete(Adafruit_Fingerprint &finger, const char *bitmap_file, int number);   // tested
 int finger_search(Adafruit_Fingerprint &finger, const char *bitmap_file);   // tested
 
-void printLocalTime() {
+void check_in_print(int ID) {
+  char time_str[64];
   struct tm timeinfo;
-  if (!getLocalTime(&timeinfo)) {
-    debug_print("fetch time failed");
-    return;
+  if(getLocalTime(&timeinfo))
+    strftime(time_str, sizeof(time_str),"%Y-%m-%d %H:%M:%S", &timeinfo);
+  else {
+    strcpy(time_str, "fetch time failed");
+    debug_print("check_in_print: fetch time failed");
   }
-  Serial.println(&timeinfo, "time: %Y-%m-%d %H:%M:%S");
-}
-
-int time2str(char *time_str) {
-  struct tm timeinfo;
-  if (!getLocalTime(&timeinfo)) {
-    debug_print("fetch time failed");
-    return -1;
-  }
-  sprintf(time_str, "%Y-%m-%d %H:%M:%S", &timeinfo);
-  return 0;
+  File f = SD.open(CHECKINFILE, FILE_APPEND);
+  f.print(time_str);
+  f.print(" -> ");
+  f.println(ID);
+  f.close();
 }
 
 /**
@@ -92,56 +90,49 @@ void setup()
   // debug uart port init
   Serial.begin(115200); // serial for debug
   while (!Serial)
-  { // wait debug serial
     yield();
-  }
 
   // ssd1306 screen init
   if (u8g2.begin())
-  {
     debug_print("ssd1306 init successed");
-  }
   else
-  {
     panic("ssd1306 init failed!");
-  }
 
   // wifi init
   WiFi.mode(WIFI_AP_STA);
   WiFi.begin(WIFI_ID, WIFI_PWD);
   while(WiFi.status() != WL_CONNECTED) delay(500);
   debug_print("STA Mode IP: %s", wifi_addr_trans(WiFi.localIP()));
+  configTime(utcOffsetInSeconds, 0, ntpServer);
 
   // fingerprint init
   Serial2.begin(57600, SERIAL_8N1, 16, 17); // serial for AS608
   if (finger.verifyPassword())
-  {
     debug_print("fingerprint init success!");
-  }
   else
-  {
     panic("fingerprint init failed");
-  }
 
   // spi sdcard module init
-  if (SD.begin())
-  { // dont need to specify the spi bus, use spi0 as default
+  if (SD.begin()) {
     debug_print("sdcard init success!");
+    ssd1306_print("sdcard found");
   }
-  else
-  {
+  else{
+    ssd1306_print("where's your sdcard");
     panic("sdcard init failed!");
   }
 
   // create checkin file and bitmap
-  if (!SD.exists(CHECKINFILE))
-  {
+  if (!SD.exists(CHECKINFILE)) {
+    ssd1306_print("no check-in file, creating...");
+    delay(200);
     File file = SD.open(CHECKINFILE, FILE_WRITE);
     file.println("start check-in");
     file.close();
   }
-  if (!SD.exists(BITMAPFILE))
-  {
+  if (!SD.exists(BITMAPFILE)) {
+    ssd1306_print("no bitmap file, creating...");
+    delay(200);
     create_bitmap(BITMAPFILE);
     read_bitmap(BITMAPFILE, bmap);
     debug_print("bitmap: %s", bmap);
@@ -149,20 +140,53 @@ void setup()
   }
 
   debug_print("All hardware init success");
-  ssd1306_print(0, 0, "All hardware init success");
-
-  configTime(utcOffsetInSeconds, 0, ntpServer);
-  printLocalTime();
+  ssd1306_print("All hardware init success");
+  delay(200);
+#ifdef DEBUG
+  read_bitmap(BITMAPFILE, bmap);
+  debug_print("bitmap ->");
+  debug_print("%s", bmap);
+#endif
+  //test1();
+  //test2();
+  //test3();
+  //printLocalTime();
 }
 
 void loop()
 {
-  finger_enroll(finger, BITMAPFILE);
-  debug_print("wait");
-  delay(2000);
-  finger_delete(finger, BITMAPFILE, 1);
-  debug_print("%s", bmap);
-  while(1);
+  debug_print("please select mode");
+  ssd1306_print("select mode");
+  int delNum = -1;
+  int mode = Serial2Int();
+  switch(mode) {
+    case(1):    // enroll mode
+      debug_print("enroll_mode!!!");
+      ssd1306_print("enroll_mode!!!");
+      delay(2000);
+      finger_enroll(finger, BITMAPFILE);
+      break;
+    case(2):    // check-in mode
+      debug_print("check_in mode!!!");
+      ssd1306_print("check_in mode!!!");
+      delay(2000);
+      check_in(finger, BITMAPFILE);
+      break;
+    case(3): {   // delete mode
+      debug_print("delete_mode!!!");
+      ssd1306_print("delete_mode!!!");
+      delay(2000);
+      debug_print("input number to del");
+      ssd1306_print("input number to del");
+      int delNum = Serial2Int();
+      finger_delete(finger, BITMAPFILE, delNum);
+      break;
+    }
+    default:
+      debug_print("invalid mode input");
+      ssd1306_print("invalid mode input");
+      break;
+  }
 }
 
 /**
@@ -185,11 +209,12 @@ int finger_enroll(Adafruit_Fingerprint &finger, const char *bitmap_file) {
   }
 
   int p = -1;
-  debug_print("Waiting for valid finger to enroll as #%d", id);
+  debug_print("Waiting for finger to enroll as #%d", id);
   while (p != FINGERPRINT_OK) {
     p = finger.getImage();
     switch (p) {
     case FINGERPRINT_OK:
+      debug_print("");
       debug_print("Image taken");
       break;
     case FINGERPRINT_NOFINGER:
@@ -229,6 +254,12 @@ int finger_enroll(Adafruit_Fingerprint &finger, const char *bitmap_file) {
     default:
       debug_print("Unknown error");
       return p;
+  }
+
+  p = finger.fingerFastSearch();
+  if(p == FINGERPRINT_OK && (bmap[finger.fingerID] == '1')) {
+    debug_print("finger_enroll: finger has been enrolled");
+    return -1;
   }
 
   debug_print("Remove finger");
@@ -323,6 +354,9 @@ int finger_enroll(Adafruit_Fingerprint &finger, const char *bitmap_file) {
   bmap[id - 1] = '1';
   debug_print("%s", bmap);
   write_bitmap(bitmap_file, bmap);
+
+  ssd1306_print("your ID: %d", id);
+  delay(5000);
   return true;
 }
 
@@ -336,23 +370,23 @@ int finger_enroll(Adafruit_Fingerprint &finger, const char *bitmap_file) {
  */
 int finger_delete(Adafruit_Fingerprint &finger, const char *bitmap_file, int id)
 {
-  //debug_print("put your finger on");
-  ssd1306_print(0, 0, "put your finger on");
   int check_id = finger_search(finger, bitmap_file);
   if(check_id != id) {
     debug_print("finger_delete: finger does not match");
-    ssd1306_print(0, 0, "finger does not match");
+    ssd1306_print("finger does not match");
     return -1;
   }
   finger.deleteModel(id);
   read_bitmap(bitmap_file, bmap);
   bmap[id-1] = '0';
   write_bitmap(bitmap_file, bmap);
-  //ssd1306_print(display, 0, 0, "finger deleted");
-  #ifdef DEBUG
+  // print info
+  debug_print("finger_delete: finger deleted");
+  ssd1306_print("finger deleted");
+#ifdef DEBUG
   debug_print("bitmap after deleted:");
   debug_print("%s", bmap);
-  #endif
+#endif
   return 0;
 }
 
@@ -368,15 +402,16 @@ int finger_search(Adafruit_Fingerprint &finger, const char *bitmap_file) {
   // if finger can be searched, return number
   // else return -1
   int p = -1;
+  debug_print("finger_search: waiting for finger");
+  ssd1306_print("waiting for finger");
   while((p = finger.getImage()) != FINGERPRINT_OK) {
     // will add something later
-    debug_print("please put you finger on the sensor");
     switch (p) {
       case FINGERPRINT_OK:
         debug_print("Image taken");
         break;
       case FINGERPRINT_NOFINGER:
-        debug_print(".");
+        Serial.print(".");
         break;
       case FINGERPRINT_PACKETRECIEVEERR:
         debug_print("Communication error");
@@ -389,6 +424,7 @@ int finger_search(Adafruit_Fingerprint &finger, const char *bitmap_file) {
         break;
     }
   }
+  debug_print("");  // enter
 
   p = finger.image2Tz();
   if (p != FINGERPRINT_OK) {
@@ -412,28 +448,26 @@ int finger_search(Adafruit_Fingerprint &finger, const char *bitmap_file) {
 }
 
 /**
- * @brief get the real time and write check-in log
+ * @brief check the finger, get the real time and write check-in log
  * 
  * @param number
  * @param file 
  */
-void check_in(Adafruit_Fingerprint finger, const char *file) {
-  File f = SD.open(file, FILE_WRITE);
+void check_in(Adafruit_Fingerprint &finger, const char *bitmap_file) {
+  read_bitmap(bitmap_file, bmap);
   while(1) {
     int ID = -1;
-    if((ID = finger_search(finger, BITMAPFILE)) < 0) {
+    if((ID = finger_search(finger, bitmap_file)) < 0) {
       debug_print("check-in: finger not right");
+      delay(2000);
       continue;
     }
-    char timestr[15];
-    if (time2str(timestr) < 0) {
-      debug_print("check-in: unable to fetch the time");
-      continue;
-    }
-    String checkinfo;
-    checkinfo = String(ID) + timestr;
+    char time_str[16];
+    check_in_print(ID);
+    debug_print("ID: %d, check-in success", ID);
+    ssd1306_print("ID: %d, check-in success", ID);
+    delay(2000);
   }
-  f.close();
 }
 
 /**
@@ -481,6 +515,41 @@ int write_bitmap(const char *file, char *bitmap) {
   return 0;
 }
 
+/**
+ * @brief print local time
+ * 
+ */
+void printLocalTime() {
+  struct tm timeinfo;
+  if (!getLocalTime(&timeinfo)) {
+    debug_print("fetch time failed");
+    return;
+  }
+  Serial.println(&timeinfo, "time: %Y-%m-%d %H:%M:%S");
+}
+
+/**
+ * @brief get time info to string format
+ * 
+ * @param time_str 
+ * @return int 
+ */
+int time2str(char *time_str) {
+  struct tm timeinfo;
+  if (!getLocalTime(&timeinfo)) {
+    debug_print("fetch time failed");
+    return -1;
+  }
+  sprintf(time_str, "%Y-%m-%d %H:%M:%S", &timeinfo);
+  return 0;
+}
+
+/**
+ * @brief get string ip addr
+ * 
+ * @param ip 
+ * @return String 
+ */
 String wifi_addr_trans(int ip) {
   int seg;
   String ret;
@@ -500,60 +569,49 @@ String wifi_addr_trans(int ip) {
  * @param char* fmt
  * @param ...
  */
-void ssd1306_print(int x, int y, const char* fmt, ...) {
-  char buf[19];
+void ssd1306_print(const char* fmt, ...) {
+  char buf[52];
   va_list args;
   va_start(args, fmt);
   vsnprintf(buf, sizeof(buf), fmt, args);
   va_end(args);
+  // set screen
   u8g2.clearBuffer();
   u8g2.setFont(u8g2_font_9x15_tr);
-  u8g2.setCursor(x, y + 14);  // 设置光标位置(x,y)
-  u8g2.println(buf);
+  //u8g2.setCursor(0, 14);  // 设置光标位置(x,y)
+  // set buf
+  int sz = strlen(buf);
+  int tmp = sz;
+  int n = 0;
+  while(tmp > 0) {
+    char line[15];
+    int len = (tmp < 14)? tmp : 14;
+    strncpy(line, buf + (sz - tmp), len);
+    line[len] = '\0';
+    u8g2.setCursor(0, (n + 1) * 14);
+    u8g2.println(line);
+    tmp -= 14;
+    n++;
+  }
   u8g2.sendBuffer();
 }
 
 /**
- * @brief print str in the middle of display  
+ * @brief get string from serial, turn it to int, and return that int
  * 
- * @param x 
- * @param y 
- * @param fmt 
- * @param ... 
+ * @return int 
  */
-//void ssd1306_print(int x, int y, const char *fmt, ...) {
-//  _ssd1306_print(x, y, fmt, ...);
-//  // test part
-//  //return ;
-//  // push fmt to buffer
-//  char buf[64]; 
-//  va_list args;
-//  va_start(args, fmt);
-//  vsnprintf(buf, sizeof(buf), fmt, args);
-//  va_end(args);
-//  // split to words
-//  std::string bufferStr(buf);
-//  std::istringstream iss(buf); 
-//  std::vector<std::string> words;
-//  std::string word;
-//  while(iss >> word) {
-//    words.push_back(word);
-//  }
-//  // cal cols and lines
-//  int cols = (SSD1306_WIDTH - 2 * x) / 9;
-//  int lines = (SSD1306_HEIGHT - 2 * y) / 15;
-//  // push words to
-//  int words_idx = 0;
-//  std::vector<std::string> screen_buffer[lines];
-//  while(1) {
-//    screen_buffer[0] += " ";
-//  }
-//  // clear screen
-//  u8g2.clearBuffer();
-//  u8g2.setFont(u8g2_font_9x15_tr);
-//  //
-//
-//}
+int Serial2Int() {
+  String inputStr;
+  char inputChar;
+  while(inputChar != '\n') {
+    while(!Serial.available()) delay(100);
+    inputChar = Serial.read();
+    if(inputChar != '\n') 
+      inputStr += inputChar;
+  }
+  return inputStr.toInt();
+}
 
 /**
  * @brief print a string to default Serial like "printf()" to debug program
